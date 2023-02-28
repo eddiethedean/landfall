341a37c7cd586f8a1b77cb4677b5784daaae1802
Odos Matthews <odosmatthews@Odoss-MacBook-Pro.local>
2023-02-28 14:53:02 -0500

    add partial plot_points function

--- src/landfall/points.py
+++ src/landfall/points.py
@@ -1,3 +1,33 @@
 """
 Functions for plotting points.
 """
+
+import staticmaps
+
+
+tp = staticmaps.tile_provider_OSM
+
+
+def plot_points(
+    lats,
+    lons,
+    tile_provider=tp,
+    point_size=10,
+    window_size=(500, 400),
+    zoom=0,
+    color=staticmaps.color.BLUE,
+    set_zoom=None,
+    center=None
+):
+    context = staticmaps.Context()
+    context.set_tile_provider(tile_provider)
+
+    for lat, lon in zip(lats, lons):
+        point = staticmaps.create_latlng(lat, lon)
+        marker = staticmaps.Marker(point, color=color, size=point_size)
+        context.add_object(marker)
+        _center, _zoom = context.determine_center_zoom(*window_size)
+        context.set_zoom(_zoom + zoom)
+        if center is not None:
+            if type(center) is tuple:
+                point = staticmaps.create_latlng(*center)
